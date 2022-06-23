9e031775bc163c0a722b0a7cdea79dae32d2afac
Calvi <calvi.alejo@gmail.com>
2022-06-23 14:11:25 -0300

    add: logo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,19 @@ import React from 'react';
 import { ChakraProvider, Divider, Box, Container, VStack, Image, Text, Heading } from '@chakra-ui/react';
 import {AppProps} from "next/app";
 
+
+
 import theme from "../theme";
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={theme}>
-      <Box padding={4}>
+      <Box padding={4} justifyItems={"center"} alignItems={"center"}>
         <Container borderRadius={"sm"} backgroundColor={"white"} boxShadow={"md"} marginY={4} maxW={"container.xl"} padding={4}>
           <VStack marginBottom={6}>
-            <Image borderRadius={9999} src="//placehold.it/128x128"></Image>
-            <Heading>Santa Rita</Heading>
-            <Text>Pescadería</Text>
+            <Image borderRadius={9999} src="https://i.ibb.co/YybqGKS/logo.png" alt="Logo"></Image>
+            <Heading>Pedidos</Heading>
+            {/* <Text>Pedidos</Text> */}
           </VStack>
           <Divider marginY={6}/>
           <Component {...pageProps} />
